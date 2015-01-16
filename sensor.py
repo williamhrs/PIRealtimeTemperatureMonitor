46c5313c0e1967ffd4f23a35c9beb656c9606478
William Silva <williamhrs@Williams-MacBook-Pro.local>
2015-01-16 16:40:11 -0200

    initial commit

--- sensor.py
+++ sensor.py
@@ -1,30 +1,33 @@
 import time
-from urllib2 import Request, urlopen
+import urllib2 
 import json
 #import Adafruit_MCP9808.MCP9808 as MCP9808
 #sensor = MCP9808.MCP9808()
 
-#sensor.begin()
+
+
+#httplib.HTTPSConnection.debuglevel = 1
+
+sensor.begin()
 
 timestamp = lambda: int(round(time.time() * 1000))
 
-#temp = sensor.readTempC()
-temp = 2
-values = { 'data': { 'temp' : temp, 'timestamp' : timestamp() } }
-# REST API uses an additional header - "Appbase-Secret"
-headers = {
-  'Content-Type': 'application/json',
-  'Appbase-Secret': '9d7f14bc1ecabc8b47ed176e4e1772cd'
-}
-                              
-# Send "PATCH" request to create or update a resource.
-request = Request('https://api.appbase.io/tempmonitor/v2/pi/temperature', data=json.dumps(values), headers=headers)
-request.get_method = lambda: 'PATCH'
-x = urlopen(request)
-print x.read()
+while True:
+	temp = sensor.readTempC()
 
-#while True:
-#    temp = sensor.readTempC()
-#    print temp
-#    time.sleep(5.0)
-	
+	values = { 'data': { timestamp() : {"path":"temperature/"+timestamp() } } }
+	# REST API uses an additional header - "Appbase-Secret"
+	headers = {
+	  'Content-Type': 'application/json',
+	  'Appbase-Secret': '9d7f14bc1ecabc8b47ed176e4e1772cd'
+	}
+	                              
+	# Send "PATCH" request to create or update a resource.
+	request = urllib2.Request('https://api.appbase.io/tempmonitor/v2/pi/temperature/~edges', data=json.dumps(values), headers=headers)
+	request.get_method = lambda: 'PATCH'
+	try:
+		x = urllib2.urlopen(request)
+		print x.read()
+	except urllib2.HTTPError, e:
+		print e.hdrs
+	time.sleep(5.0)
