69204706bf4f2f06e047e87273a4bdf0fb449c50
William Silva <williamhrs@Williams-MacBook-Pro.local>
2015-01-16 16:59:57 -0200

    initial commit

--- sensor.py
+++ sensor.py
@@ -11,23 +11,13 @@ timestamp = lambda: int(round(time.time() * 1000))
 while True:
 	temp = sensor.readTempC()
 	curTimeStamp = str(timestamp())
-	values = { 'data': { curTimeStamp : {"path":"pi/temperature/" } } }
+
 	# REST API uses an additional header - "Appbase-Secret"
 	headers = {
 	  'Content-Type': 'application/json',
 	  'Appbase-Secret': '9d7f14bc1ecabc8b47ed176e4e1772cd'
 	}
-	                              
-	# Send "PATCH" request to create an edge.
-	request = urllib2.Request('https://api.appbase.io/tempmonitor/v2/pi/temperature/~edges', data=json.dumps(values), headers=headers)
-	request.get_method = lambda: 'PATCH'
-	try:
-		x = urllib2.urlopen(request)
-		print x.read()
-		print "leitura"
-	except urllib2.HTTPError, e:
-		print e
-	
+
 	values = { 'data': { 'temperature' : temp, 'timestamp':curTimeStamp } }
 	# Send "PATCH" request to update properties
 	request = urllib2.Request('https://api.appbase.io/tempmonitor/v2/pi/temperature/'+curTimeStamp+'/~properties', data=json.dumps(values), headers=headers)
@@ -38,5 +28,17 @@ while True:
 		print "leitura2"
 	except urllib2.HTTPError, e:
 		print e
-	time.sleep(5.0)
+	
+	
+	values = { 'data': { curTimeStamp : {"path":"pi/temperature/" } } }	                              
+	# Send "PATCH" request to create an edge.
+	request = urllib2.Request('https://api.appbase.io/tempmonitor/v2/pi/temperature/~edges', data=json.dumps(values), headers=headers)
+	request.get_method = lambda: 'PATCH'
+	try:
+		x = urllib2.urlopen(request)
+		print x.read()
+		print "leitura"
+	except urllib2.HTTPError, e:
+		print e
 
+	time.sleep(5.0)
